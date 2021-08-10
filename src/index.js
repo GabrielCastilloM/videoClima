4ae6bac2e67177348e28ffb1b2fdcc4f4ce80938
GabrielCastilloM <jgabrielcastillo@gmail.com>
2021-08-09 21:05:59 -0500

    validacion regiones consultadas

--- src/index.js
+++ src/index.js
@@ -30,57 +30,73 @@ btnMute.addEventListener('click', mutear)
 btnBig.addEventListener('click', hacerGrandeVideo  )
 
 //Reto consumir api de clima openweathermap.org
-  const obtenerDatos = (ciudad) => {
-    const   KEY_API = 'dd130c00b823cfbdd95cc1d732944e50'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${KEY_API}`;
-    const arrayDatos = fetch(url).then(res => res.json())
-    document.getElementById("ciudad").value = "";
-    arrayDatos.then((datos) => {
-      pintarInformacion(datos)
-    })
-    .catch(() => {
-      swal("Pais o Ciudad no encontrada", {
-        icon: "error",
-      });
-    })
+//-----------------------------------------------------------------------------------
+let btnconsulta = document.getElementById("generar");
+btnconsulta.addEventListener("click", () => {
+let inputCiudad = document.getElementById("ciudad");
+let valor = inputCiudad.value;
+if (valor == '') {
+  swal("Por Favor Digitar Pais o Ciudad", {
+    icon: "info",
+  });
+  } else {
+    validarRegionesConsultadas(valor)
   }
+})
 
-  var btnconsulta = document.getElementById("generar");
-  btnconsulta.addEventListener("click", () => {
-  var inputCiudad = document.getElementById("ciudad");
-  var valor = inputCiudad.value;
-  if (valor == '') {
-    swal("Por Favor Digitar Pais o Ciudad", {
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+    let inputCiudad = document.getElementById("ciudad");
+    let valor = inputCiudad.value;
+    if (valor == '') {
+      swal("Por Favor Digitar Pais o Ciudad", {
+        icon: "info",
+      });
+      } else {
+        validarRegionesConsultadas(valor)
+      }
+    }
+  })
+
+let regionesconsultadas = [];
+
+function validarRegionesConsultadas(ciudad) {
+  if (regionesconsultadas.includes( ciudad )) {
+    swal("Ya consultaste esta región", {
       icon: "info",
     });
-    } else {
-      obtenerDatos(valor)
-    }
-  })
+  document.getElementById("ciudad").value = "";
+  } else {
+  obtenerDatos(ciudad)
+  }
+}
 
-  document.addEventListener("keyup", (event) => {
-    if (event.code === "Enter") {
-      var inputCiudad = document.getElementById("ciudad");
-      var valor = inputCiudad.value;
-      obtenerDatos(valor)
-    }
+const obtenerDatos = (ciudad) => {
+  const   KEY_API = 'dd130c00b823cfbdd95cc1d732944e50'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${KEY_API}`;
+  const arrayDatos = fetch(url).then(res => res.json())
+  document.getElementById("ciudad").value = "";
+  arrayDatos.then((datos) => {
+    regionesconsultadas.push(ciudad);
+    pintarInformacion(datos)
   })
+  .catch(() => {
+    swal("Pais o Ciudad no encontrada", {
+      icon: "error",
+    });
+  })
+}
 
 const pintarInformacion = (datos) => {
   const papa = document.querySelector('.contenedor__seccion2--consulta')
   papa.insertAdjacentHTML('beforeend', userTemplate(datos));
-
 }
   //funcion crea el template de la tarjeta
   function userTemplate(datos) {
-    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-      datos.weather[0]["icon"]
-    }.svg`;
-
+    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${datos.weather[0]["icon"]}.svg`;
     const temperatura = datos.main.temp
     const temperaturaConvertidaString = temperatura.toString()
     const temperatoraAGrados = temperaturaConvertidaString.substring(0, temperaturaConvertidaString.length - 4)
-
     return `
     <div class="card mb-2 shadow">
           <div class="card-body">
